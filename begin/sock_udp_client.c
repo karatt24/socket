856587b702bf333b13bf850381372f0579efe49e
karatt24 <karatt1987@yandex.ru>
2017-07-17 11:21:49 +0600

    Набросок

---
+++ begin/sock_udp_client.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <unisyd.h>
+#include <errno.h>
+#include <sys/un.h>
+#include <string.h>
+
+
+#define SOCKET_ADDR "soket.socket"
+
+
+int main(){
+
+	int sock, len;
+	struct sockaddr_un addr, client;
+	char buf[50];
+	char name[20]="Connection";
+	sock = soket(AF_LOLOCAL, SOCK_DGRAM, 0);
+	if(sock < 0){
+		perror("socket");
+		exit(1);
+	}
+	addr.sun_family = AF_LOCAL;
+	addr.sun_path = "./socket";
+
+	
+
+}
